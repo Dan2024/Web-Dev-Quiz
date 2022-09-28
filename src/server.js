98272951be5d33c76d86bc9d8fb89db28fe68356
Tibor22 <tothtibitravel@gmail.com>
2022-09-28 15:23:03 +0100

    implemented google auth

--- src/server.js
+++ src/server.js
@@ -1,37 +1,42 @@
-import express from 'express'
-import helmet from 'helmet'
-import passport from 'passport'
-import cookieSession from 'cookie-session'
-import cors from 'cors'
-import './services/passport.js'
-import authRoute from './routes/auth.js'
-import userRoute from './routes/user.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import express from 'express';
+import helmet from 'helmet';
+import passport from 'passport';
+import cookieSession from 'cookie-session';
+import cors from 'cors';
+import './services/passport.js';
+import authRoute from './routes/auth.js';
+import userRoute from './routes/user.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config = {
-  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
-}
+	CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+	CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+	COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+	COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
 
-const app = express()
-app.use(helmet())
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+const app = express();
+app.use(helmet());
 app.use(
-  cookieSession({
-    name: 'session',
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
-  })
-)
-app.use(passport.initialize())
-app.use(passport.session())
-app.use('/auth', authRoute)
-app.use('/user', userRoute)
+	cors({
+		credentials: true,
+		origin: ['http://localhost:3000', 'http://localhost:3000/lobby'],
+	})
+);
+app.use(
+	cookieSession({
+		name: 'session',
+		maxAge: 24 * 60 * 60 * 1000,
+		keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/auth', authRoute);
+app.use('/user', userRoute);
 
-const port = 4000
+const port = 4000;
 app.listen(port, () => {
-  console.log(`\n Server is running on port ${port}\n`)
-})
+	console.log(`\n Server is running on port ${port}\n`);
+});
--- src/services/passport.js
+++ src/services/passport.js
@@ -1,64 +1,65 @@
-import passport from 'passport'
-import { Strategy } from 'passport-google-oauth20'
-import dbClient from '../utils/dbClient.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import passport from 'passport';
+import { Strategy } from 'passport-google-oauth20';
+import dbClient from '../utils/dbClient.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config = {
-  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
-}
+	CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+	CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+	COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+	COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
 
 const AUTH_OPTIONS = {
-  callbackURL: '/auth/google/callback',
-  clientID: config.CLIENT_ID,
-  clientSecret: config.CLIENT_SECRET,
-}
+	callbackURL: '/auth/google/callback',
+	clientID: config.CLIENT_ID,
+	clientSecret: config.CLIENT_SECRET,
+};
 
 async function verifyCallback(accessToken, refreshToken, profile, done) {
-  const user = await dbClient.user.findUnique({
-    where: { googleId: profile.id },
-  })
+	console.log(profile);
+	const user = await dbClient.user.findUnique({
+		where: { googleId: profile.id },
+	});
 
-  if (!user) {
-    const createdUser = await dbClient.user.create({
-      data: {
-        googleId: profile.id,
-        email: profile.emails[0].value,
-        name: profile.displayName,
-        img: profile.photos[0].value,
-      },
-    })
-    profile.googleID = profile.id
-    profile.id = createdUser.id
-  } else {
-    profile.googleID = profile.id
-    profile.id = user.id
-  }
+	if (!user) {
+		const createdUser = await dbClient.user.create({
+			data: {
+				googleId: profile.id,
+				email: profile.emails[0].value,
+				name: profile.displayName,
+				img: profile.photos[0].value,
+			},
+		});
+		profile.googleID = profile.id;
+		profile.id = createdUser.id;
+	} else {
+		profile.googleID = profile.id;
+		profile.id = user.id;
+	}
 
-  done(null, profile)
+	done(null, profile);
 }
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 // Save the session to the cookie
 passport.serializeUser((user, done) => {
-  done(null, {
-    googleID: user.googleID,
-    id: user.id,
-    stat: user.stat ? user.stat : {},
-    email: user.emails[0].value,
-    name: user.displayName,
-    img: user.photos[0].value,
-  })
-})
+	done(null, {
+		googleID: user.googleID,
+		id: user.id,
+		stat: user.stat ? user.stat : {},
+		email: user.emails[0].value,
+		name: user.displayName,
+		img: user.photos[0].value,
+	});
+});
 
 // Read the session from the cookie
 passport.deserializeUser((obj, done) => {
-  // User.findById(id).then(user => {
-  //   done(null, user);
-  // });
-  done(null, obj)
-})
+	// User.findById(id).then(user => {
+	//   done(null, user);
+	// });
+	done(null, obj);
+});
