fda64c6a08d91c2815504561cfe224f7bcdc08f9
Tibor22 <tothtibitravel@gmail.com>
2022-09-30 17:14:35 +0100

    question

--- src/domain/question.js
+++ src/domain/question.js
@@ -1,13 +1,13 @@
-import dbClient from '../utils/dbClient.js'
+import dbClient from '../utils/dbClient.js';
 
 export default async function getQuestions(category, numOfQuestions) {
-  const questionsByCategory = await dbClient.question.findMany({
-    where: { category: category },
-    include: { answer: true },
-  })
+	const questionsByCategory = await dbClient.question.findMany({
+		where: { category: category },
+		include: { answer: true },
+	});
 
-  const shuffledQuestions = questionsByCategory.sort(() => 0.5 - Math.random())
-  const questions = shuffledQuestions.slice(0, numOfQuestions)
+	const shuffledQuestions = questionsByCategory.sort(() => 0.5 - Math.random());
+	const questions = shuffledQuestions.slice(0, numOfQuestions);
 
-  return questions
+	return questions;
 }
