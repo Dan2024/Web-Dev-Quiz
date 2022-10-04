5a6972bc539b14db358026f4ba88f00115920c87
Dan <dannorthcott8@gmail.com>
2022-10-04 19:58:16 +0100

    reformatted questions returned from question endpoint

--- src/domain/question.js
+++ src/domain/question.js
@@ -1,13 +1,38 @@
-import dbClient from '../utils/dbClient.js'
+import dbClient from '../utils/dbClient.js';
 
 export default async function getQuestions(category, numOfQuestions) {
-  const questionsByCategory = await dbClient.question.findMany({
-    where: { category: category },
-    include: { answer: true, options: true },
-  })
+    const questionsByCat = await dbClient.question.findMany({
+        where: { category: category },
+        include: { answer: true, options: true },
+    });
 
-  const shuffledQuestions = questionsByCategory.sort(() => 0.5 - Math.random())
-  const questions = shuffledQuestions.slice(0, numOfQuestions)
+    const shuffledQuestions = questionsByCat.sort(() => 0.5 - Math.random());
+    const questions = shuffledQuestions.slice(0, numOfQuestions);
+    const formattedQuestions = questions.map((question) => {
+        return {
+            id: question.id,
+            text: question.text,
+            answer: question.answer.text,
+            options: shuffle([...question.options, question.answer]),
+        };
+    });
 
-  return questions
+    return formattedQuestions;
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
+
+    return array;
 }
